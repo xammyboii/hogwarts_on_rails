50483d17709d26d9950f570473c82c2224dcd829
xammyboii <xavieryabados.09@gmail.com>
2023-10-06 00:44:18 -0500

    generated a houses controller with index and show action/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'houses/index'
+  get 'houses/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
