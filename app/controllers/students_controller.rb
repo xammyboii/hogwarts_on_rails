a8f1715523f9dd6e1fc6aabf2edbc29ae5671c43
xammyboii <xavieryabados.09@gmail.com>
2023-10-18 23:45:36 -0500

    implemented students show action/view and fix N+1 problem

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,17 @@
 class StudentsController < ApplicationController
   # GET /students
   def index
-    @students = Student.order(:name)
+    # The .includes removes our N+1 issue when
+    # accessing the student houses in a loop in the view.
+    @students = Student.includes(:house).order(:name)
   end
   # Automagically loads the associated view:
   # app/views/students/index.html.erb
 
-  # GET /students/:id
+  # GET /students/:id   => params[:id]
   def show
-
+    @student = Student.find(params[:id])
   end
+  # Automagically loads the associated view:
+  # app/views/students/show.html.erb
 end
