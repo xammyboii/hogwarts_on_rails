410c5b69d36633c03805191c152cf389c4e417fc
xammyboii <xavieryabados.09@gmail.com>
2023-10-18 23:20:32 -0500

    implemented the student index action/view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,13 @@
 class StudentsController < ApplicationController
+  # GET /students
   def index
+    @students = Student.order(:name)
   end
+  # Automagically loads the associated view:
+  # app/views/students/index.html.erb
 
+  # GET /students/:id
   def show
+
   end
 end
